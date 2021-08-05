4fe7f867991d1571a7ff8e4bed605b2f070f8cf3
jawad <jawadhalbymakary@gmail.com>
2021-08-05 05:03:37 +0300

    explore nearby section added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
+import SmallCard from '../components/SmallCard'
+
 import Header from '../components/Header'
 
-export default function Home() {
+export default function Home({exploreData}) {
   
 //npx create-next-app -e with-tailwindcss my-project to create it
 
@@ -10,6 +12,8 @@ export default function Home() {
   //delete main and footer
   //head cmp is from nextJs
   //same as head in html5
+  // sm -> bigger then mobile screen
+ 
   return (
     <div className="">
       <Head>
@@ -19,6 +23,49 @@ export default function Home() {
        
       <Header/>
       <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+          <section className="pt-6">
+            <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+            <div className="grid
+            sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
+            ">
+                {exploreData?.map(({img,distance,location})=>(
+                <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+
+                />
+              ))}
+            </div>
+{/* ?.->optional chaining */}
+        
+          {/* pull data from server - api endpoints(static rendering is used) */}
+          {/* if data always change we need to use server side rendering */}
+         {/* When user tried to load the site,
+          user make a request to server and server deliver a page
+         */}
+          {/* faster load and better UX */}
+
+          </section>
+      </main>
     </div>
   )
+}
+// we need to create & export  getStaticProps
+export async function getStaticProps(){
+// fetch some info to build that page
+//exploreData->responsible for data
+const exploreData=await fetch('https://links.papareact.com/pyp')
+.then((res)=>res.json()
+)
+// we need to return it to the ftc component
+return {
+  props:{
+    exploreData:exploreData
+  }
+}
+
+
 }
